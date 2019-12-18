71dc9396ab60f8a97fac220d339139cfadb7606b
Aliaksandr.Drabkou <a2.drabkov@gmail.com>
2019-12-18 14:24:47 +0300

    add contract test done + ONA

--- src/main/java/de/alectogmbh/friendsurance/automation/pages/fsb/web/FsbOverallNeedAnalysisPage.java
+++ src/main/java/de/alectogmbh/friendsurance/automation/pages/fsb/web/FsbOverallNeedAnalysisPage.java
@@ -1,14 +1,13 @@
 package de.alectogmbh.friendsurance.automation.pages.fsb.web;
 
 import de.alectogmbh.friendsurance.automation.pages.AbstractPageObject;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.At;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 import static de.alectogmbh.friendsurance.automation.pages.fsb.web.FsbOverallNeedAnalysisPage.ONA_QUESTIONNAIRE_PAGE_URL;
 import static de.alectogmbh.friendsurance.automation.utils.SeleniumWaitUtils.waitForAnimationToFinish;
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 
 @At("#HOST" + ONA_QUESTIONNAIRE_PAGE_URL)
 @DefaultUrl(ONA_QUESTIONNAIRE_PAGE_URL)
@@ -16,106 +15,87 @@ public class FsbOverallNeedAnalysisPage extends AbstractPageObject {
 
     final static String ONA_QUESTIONNAIRE_PAGE_URL = "/overallneedanalysis/";
 
-    @FindBy (id = "ONA_MARITAL_STATUS_SINGLE__label")
-    private WebElement onaMaritalStatusSingleOption;
+    @FindBy(id = "ONA_MARITAL_STATUS_SINGLE__label")
+    private WebElementFacade onaMaritalStatusSingleOption;
 
     @FindBy(id = "dashboard-questionnaire-step-next-save__button")
-    private WebElement onaSaveFamilyStatusAndGoToNextStep;
+    private WebElementFacade onaSaveFamilyStatusAndGoToNextStep;
 
     @FindBy(id = "ONA_DEPENDENT_CHILDREN")
-    private WebElement numberOfChildrenDropdown;
+    private WebElementFacade numberOfChildrenDropdown;
 
     @FindBy(id = "dashboard-questionnaire-step-next-save__button")
-    private WebElement onaSaveChildrenStatusAndGoToNextStep;
+    private WebElementFacade onaSaveChildrenStatusAndGoToNextStep;
 
     @FindBy(id = "ONA_CURRENT_JOB_EMPLOYED__label")
-    private WebElement onaProfessionStatus;
+    private WebElementFacade onaProfessionStatus;
 
     @FindBy(id = "dashboard-questionnaire-step-next-save__button")
-    private WebElement onaSaveProfessionStatusAndGoToNextStep;
+    private WebElementFacade onaSaveProfessionStatusAndGoToNextStep;
 
     @FindBy(id = "ONA_INSURED_LEGALLY__label")
-    private WebElement publicInsurance;
+    private WebElementFacade publicInsurance;
 
     @FindBy(id = "dashboard-questionnaire-step-next-save__button")
-    private WebElement onaSaveInsuranceStatusAndGoToNextStep;
+    private WebElementFacade onaSaveInsuranceStatusAndGoToNextStep;
 
     @FindBy(id = "ONA_PRIVATE_CAR_YES__span")
-    private WebElement livingConditionCar;
+    private WebElementFacade livingConditionCar;
 
     @FindBy(id = "ONA_OWNER_OCCUPIED_HOME__span")
-    private WebElement livingConditionOwnHouse;
+    private WebElementFacade livingConditionOwnHouse;
 
     @FindBy(id = "dashboard-questionnaire-step-next-save__button")
-    private WebElement onaSaveLivingConditionStatusAndGoToSummary;
+    private WebElementFacade onaSaveLivingConditionStatusAndGoToSummary;
 
     private static final int TIME_IN_SECONDS = 5;
 
     public void selectMaritalStatusSingleOption() {
         waitForAnimationToFinish(getDriver(), TIME_IN_SECONDS, onaMaritalStatusSingleOption);
-//        scrollToElement(onaMaritalStatusSingleOption);
-//        onaMaritalStatusSingleOption.waitUntilClickable();
         onaMaritalStatusSingleOption.click();
     }
 
     public void clickOnaSaveFamilyStatusAndGoNextButton() {
-//        onaSaveFamilyStatusAndGoToNextStep.waitUntilClickable();
         onaSaveFamilyStatusAndGoToNextStep.click();
     }
 
     public void selectNumberOfChildrenByText(String selectNumberOfChildren) {
         waitForAnimationToFinish(getDriver(), TIME_IN_SECONDS, numberOfChildrenDropdown);
-//        numberOfChildrenDropdown.waitUntilPresent();
-//        numberOfChildrenDropdown.waitUntilClickable();
         numberOfChildrenDropdown.sendKeys(selectNumberOfChildren);
     }
 
     public void clickOnaSaveChildrenStatusAndGoNextButton() {
-//        scroll_element_into_view(onaSaveChildrenStatusAndGoToNextStep);
-//        onaSaveChildrenStatusAndGoToNextStep.waitUntilClickable();
         onaSaveChildrenStatusAndGoToNextStep.click();
     }
 
     public void selectProfessionStatus() {
         waitForAnimationToFinish(getDriver(), TIME_IN_SECONDS, onaProfessionStatus);
-//        scrollToElement(onaProfessionStatus);
-//        onaProfessionStatus.waitUntilClickable();
         onaProfessionStatus.click();
     }
 
     public void clickOnaSaveProfessionStatusAndGoNextButton() {
-//        scroll_element_into_view(onaSaveProfessionStatusAndGoToNextStep);
-//        onaSaveProfessionStatusAndGoToNextStep.waitUntilClickable();
         onaSaveProfessionStatusAndGoToNextStep.click();
     }
 
     public void selectInsuranceType() {
         waitForAnimationToFinish(getDriver(), TIME_IN_SECONDS, publicInsurance);
-//        scrollToElement(publicInsurance);
-//        publicInsurance.waitUntilClickable();
         publicInsurance.click();
     }
 
     public void clickOnaSaveInsuranceTypeAndGoNextButton() {
-//        scroll_element_into_view(onaSaveInsuranceStatusAndGoToNextStep);
-//        onaSaveInsuranceStatusAndGoToNextStep.waitUntilClickable();
         onaSaveInsuranceStatusAndGoToNextStep.click();
     }
 
     public void selectLivingConditionCarCheckbox() {
         waitForAnimationToFinish(getDriver(), TIME_IN_SECONDS, livingConditionCar);
-//        livingConditionCar.waitUntilClickable();
         livingConditionCar.click();
     }
 
     public void selectLivingConditionHouseCheckbox() {
-//        livingConditionOwnHouse.waitUntilClickable();
         livingConditionOwnHouse.click();
     }
 
     public void clickOnaSaveLivingConditionsAndGoToSummaryButton() {
-//        scroll_element_into_view(onaSaveLivingConditionStatusAndGoToSummary);
-//        onaSaveLivingConditionStatusAndGoToSummary.waitUntilClickable();
         onaSaveLivingConditionStatusAndGoToSummary.click();
     }
 
--- src/main/java/de/alectogmbh/friendsurance/automation/pages/fsb/web/FsbOverallNeedAnalysisSummaryPage.java
+++ src/main/java/de/alectogmbh/friendsurance/automation/pages/fsb/web/FsbOverallNeedAnalysisSummaryPage.java
@@ -1,14 +1,13 @@
 package de.alectogmbh.friendsurance.automation.pages.fsb.web;
 
 import de.alectogmbh.friendsurance.automation.pages.AbstractPageObject;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.At;
 import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 import static de.alectogmbh.friendsurance.automation.pages.fsb.web.FsbOverallNeedAnalysisSummaryPage.ONA_SUMMARY_PAGE_URL;
 import static de.alectogmbh.friendsurance.automation.utils.SeleniumWaitUtils.waitForAnimationToFinish;
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 
 @At("#HOST" + ONA_SUMMARY_PAGE_URL)
 @DefaultUrl(ONA_SUMMARY_PAGE_URL)
@@ -17,16 +16,16 @@ public class FsbOverallNeedAnalysisSummaryPage extends AbstractPageObject {
     final static String ONA_SUMMARY_PAGE_URL = "/overallneedanalysis/summary/";
 
     @FindBy(id = "intro")
-    private WebElement onaSummaryPageHeadlineText;
+    private WebElementFacade onaSummaryPageHeadlineText;
 
     @FindBy(id = "overallneedanalysis-gonext__button")
-    private WebElement onaSaveButton;
+    private WebElementFacade onaSaveButton;
 
     @FindBy(id = "overallneedanalysis-completed-intro-header")
-    private WebElement onaCompletedHeadlineText;
+    private WebElementFacade onaCompletedHeadlineText;
 
     @FindBy(id = "overallneedanalysis-completed__gonext-button")
-    private WebElement onaCompletedGoNextButton;
+    private WebElementFacade onaCompletedGoNextButton;
 
     private static final int TIME_IN_SECONDS = 5;
 
